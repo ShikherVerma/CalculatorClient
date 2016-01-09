20472c62e813e3d3f0a68398ac8d4a12817754e2
Shikher Verma <root@shikherverma.com>
2016-01-10 05:04:49 +0530

    app complete

--- app/src/main/java/com/shikherverma/calculatorclient/MainActivity.java
+++ app/src/main/java/com/shikherverma/calculatorclient/MainActivity.java
@@ -3,54 +3,97 @@ package com.shikherverma.calculatorclient;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Random;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+  public static String LOG = MainActivity.class.toString();
+  public static String URL = "http://adarshaj.cse.iitk.ac.in/operation.php/";
+  public static String REQUEST_TAG = "REQUEST_TAG";
+  EditText mInputOne;
+  EditText mInputTwo;
+  Spinner mOperator;
+  Button mCalculate;
+  TextView mResult;
+  private RequestQueue mRequestQueue;
 
-    EditText mInputOne;
-    EditText mInputTwo;
-    Spinner mOperator;
-    Button mCalculate;
-    TextView mResult;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Log.i(LOG, "onCreate started");
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+    mInputOne = (EditText) findViewById(R.id.input_one);
+    mOperator = (Spinner) findViewById(R.id.spinner);
+    mInputTwo = (EditText) findViewById(R.id.input_two);
+    mCalculate = (Button) findViewById(R.id.button);
+    mResult = (TextView) findViewById(R.id.result);
+    Log.i(LOG, "got reference to all views");
+  }
 
-        mInputOne = (EditText) findViewById(R.id.input_one);
-        mOperator = (Spinner) findViewById(R.id.spinner);
-        mInputTwo = (EditText) findViewById(R.id.input_two);
-        mCalculate = (Button) findViewById(R.id.button);
-        mResult = (TextView) findViewById(R.id.result);
+  public void onButtonClick(View v) {
+    Log.i(LOG, "Pressed Calculate");
+    if (mInputOne.getText().toString().isEmpty() || mInputTwo.getText().toString().isEmpty()) {
+      mResult.setText("Input something first");
+      return;
     }
-
-    public void onButtonClick(View v)
-    {
-        double operandOne = Integer.parseInt(mInputOne.getText().toString());
-        double operandTwo = Integer.parseInt(mInputTwo.getText().toString());
-        String operator = mOperator.getSelectedItem().toString();
-        String result = calculate(operandOne, operandTwo, operator);
-        mResult.setText(result);
+    double operandOne = Double.parseDouble(mInputOne.getText().toString());
+    double operandTwo = Double.parseDouble(mInputTwo.getText().toString());
+    String operator = mOperator.getSelectedItem().toString();
+    JSONObject jsonObject = new JSONObject();
+    try {
+      jsonObject.put("operand_1", operandOne);
+      jsonObject.put("operand_2", operandTwo);
+    } catch (JSONException e) {
+      e.printStackTrace();
     }
-
-    public String calculate(double x, double y, String operator)
-    {
-        Random random = new Random();
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("May be 0");
-        list.add("42");
-        list.add("666");
-        list.add("Do it yourself");
-        return list.get(random.nextInt(list.size()));
-    }
+    Log.i(LOG, "jsonObject " + jsonObject.toString());
+    Log.i(LOG, "url " + URL + operator);
+    JsonRequest request = new JsonObjectRequest(Request.Method.POST, URL + operator, jsonObject.toString(), new Response.Listener<JSONObject>() {
+      @Override
+      public void onResponse(JSONObject response) {
+        try {
+          String result = response.get("result").toString();
+          mResult.setText(result);
+        } catch (JSONException e) {
+          e.printStackTrace();
+          mResult.setText("Error! parsing the response!");
+        }
+      }
+    }, new Response.ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError error) {
+        if (error instanceof NoConnectionError) //can't connect to the internet
+        {
+          mResult.setText("Error! can't connect to the internet!");
+        } else //connected to internet but couldn't connect to server!!
+        {
+          mResult.setText("Error! Server revolted!");
+        }
+      }
+    });
+    request.addMarker(REQUEST_TAG);
+    mRequestQueue.add(request);
+    Log.i(LOG, "Made network request");
+    mResult.setText("calculating...");
+  }
 }
